0c237ee6a3bf9495df0e979ebe241c178188ec8a
SuiviPDR <site2218@gmail.com>
2024-06-05 21:13:44 +0100

    Update script.js

--- CRUD-Bootstrapv5-JavaScript-Local-Storage-main/script.js
+++ CRUD-Bootstrapv5-JavaScript-Local-Storage-main/script.js
@@ -1,62 +1,30 @@
+//Contact Form in PHP
 const form = document.querySelector("form"),
-  emailField = form.querySelector(".email-field"),
-  emailInput = emailField.querySelector(".email"),
-  passField = form.querySelector(".create-password"),
-  passInput = passField.querySelector(".password"),
-  cPassField = form.querySelector(".confirm-password"),
-  cPassInput = cPassField.querySelector(".cPassword");
-// Email Validtion
-function checkEmail() {
-  const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-  if (!emailInput.value.match(emaiPattern)) {
-    return emailField.classList.add("invalid"); //adding invalid class if email value do not mathced with email pattern
-  }
-  emailField.classList.remove("invalid"); //removing invalid class if email value matched with emaiPattern
-}
-// Hide and show password
-const eyeIcons = document.querySelectorAll(".show-hide");
-eyeIcons.forEach((eyeIcon) => {
-  eyeIcon.addEventListener("click", () => {
-    const pInput = eyeIcon.parentElement.querySelector("input"); //getting parent element of eye icon and selecting the password input
-    if (pInput.type === "password") {
-      eyeIcon.classList.replace("bx-hide", "bx-show");
-      return (pInput.type = "text");
+statusTxt = form.querySelector(".button-area span");
+form.onsubmit = (e)=>{
+  e.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  statusTxt.innerText = "Sending your message...";
+  form.classList.add("disabled");
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "message.php", true);
+  xhr.onload = ()=>{
+    if(xhr.readyState == 4 && xhr.status == 200){
+      let response = xhr.response;
+      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
+        statusTxt.style.color = "red";
+      }else{
+        form.reset();
+        setTimeout(()=>{
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerText = response;
+      form.classList.remove("disabled");
     }
-    eyeIcon.classList.replace("bx-show", "bx-hide");
-    pInput.type = "password";
-  });
-});
-// Password Validation
-function createPass() {
-  const passPattern =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-  if (!passInput.value.match(passPattern)) {
-    return passField.classList.add("invalid"); //adding invalid class if password input value do not match with passPattern
   }
-  passField.classList.remove("invalid"); //removing invalid class if password input value matched with passPattern
+  let formData = new FormData(form);
+  xhr.send(formData);
 }
-// Confirm Password Validtion
-function confirmPass() {
-  if (passInput.value !== cPassInput.value || cPassInput.value === "") {
-    return cPassField.classList.add("invalid");
-  }
-  cPassField.classList.remove("invalid");
-}
-// Calling Funtion on Form Sumbit
-form.addEventListener("submit", (e) => {
-  e.preventDefault(); //preventing form submitting
-  checkEmail();
-  createPass();
-  confirmPass();
-  //calling function on key up
-  emailInput.addEventListener("keyup", checkEmail);
-  passInput.addEventListener("keyup", createPass);
-  cPassInput.addEventListener("keyup", confirmPass);
-  if (
-    !emailField.classList.contains("invalid") &&
-    !passField.classList.contains("invalid") &&
-    !cPassField.classList.contains("invalid")
-  ) {
-    location.href = form.getAttribute("action");
-  }
-});
